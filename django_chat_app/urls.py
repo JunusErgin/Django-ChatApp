7d60c7ae9ce5b892491acc3ad9e67f263ea469da
Junus Ergin <junus91@googlemail.com>
2021-11-02 13:25:55 +0100

    HTML Template

--- django_chat_app/urls.py
+++ django_chat_app/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from chat.views import index
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('chat/', index),
 ]
