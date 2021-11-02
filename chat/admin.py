75595eccf3348a7b9339634653116f164dd81f2c
Junus Ergin <junus91@googlemail.com>
2021-11-02 14:48:01 +0100

    Django Admin Interface

--- chat/admin.py
+++ chat/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Message
+
+
+class MessageAdmin(admin.ModelAdmin):
+    fields = ('text','created_at', 'author', 'receiver')
+    list_display = ('created_at', 'author', 'text', 'receiver')
+    search_fields = ('text',)
 
 # Register your models here.
+admin.site.register(Message, MessageAdmin)
