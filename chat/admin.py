09350590c7b76e0e89372e569eaf4ede16524206
Junus Ergin <junus91@googlemail.com>
2021-11-02 16:10:54 +0100

    Model Fields ändern

--- chat/admin.py
+++ chat/admin.py
@@ -1,11 +1,12 @@
 from django.contrib import admin
-from .models import Message
+from .models import Chat, Message
 
 
 class MessageAdmin(admin.ModelAdmin):
-    fields = ('text','created_at', 'author', 'receiver')
+    fields = ('chat', 'text','created_at', 'author', 'receiver')
     list_display = ('created_at', 'author', 'text', 'receiver')
     search_fields = ('text',)
 
 # Register your models here.
 admin.site.register(Message, MessageAdmin)
+admin.site.register(Chat)
--- chat/models.py
+++ chat/models.py
@@ -1,12 +1,17 @@
 from django.db import models
-from django.db.models.fields import DateField
 from datetime import date
 from django.conf import settings
 
 # Create your models here.
+
+class Chat(models.Model):
+    created_at = models.DateField(default=date.today)
+
+
 class Message(models.Model):
     text = models.CharField(max_length=500)
-    created_at = DateField(default=date.today)
+    created_at = models.DateField(default=date.today)
     # chat = Chat Klasse verknüfen
+    chat = models.ForeignKey(Chat, on_delete=models.CASCADE, related_name='chat_message_set', default=None, blank=True, null=True)
     author = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='author_message_set')
     receiver = models.ForeignKey(settings.AUTH_USER_MODEL, on_delete=models.CASCADE, related_name='receiver_message_set')
