127c4156dbfaf26b5b10241ef330355f6aac3b5f
Junus Ergin <junus91@googlemail.com>
2021-11-02 16:21:08 +0100

    Datenbank Objekt erstellen

--- chat/views.py
+++ chat/views.py
@@ -1,6 +1,9 @@
 from django.shortcuts import render
+from .models import Chat, Message
 
 def index(request):
     if request.method == 'POST':
         print("Received data " + request.POST['textmessage'])
+        myChat = Chat.objects.get(id=1)
+        Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
     return render(request, 'chat/index.html', {'username': 'Junus'})
