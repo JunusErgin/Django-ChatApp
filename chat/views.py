dbc85551a52afbce127c333f2b772034fedbfbaf
Junus Ergin <junus91@googlemail.com>
2021-11-02 15:35:30 +0100

    GET vs POST request

--- chat/views.py
+++ chat/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
 
 def index(request):
+    if request.method == 'POST':
+        print("Received data " + request.POST['textmessage'])
     return render(request, 'chat/index.html', {'username': 'Junus'})
