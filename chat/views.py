21940a1c771b55541593fa1b8c79fea9ab58c0f6
Junus Ergin <junus91@googlemail.com>
2021-12-13 14:21:27 +0100

    login_required annotation

--- chat/views.py
+++ chat/views.py
@@ -2,7 +2,9 @@ from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Chat, Message
 from django.contrib.auth import authenticate, login
+from django.contrib.auth.decorators import login_required
 
+@login_required(login_url='/login/')
 def index(request):
     if request.method == 'POST':
         print("Received data " + request.POST['textmessage'])
@@ -12,12 +14,13 @@ def index(request):
     return render(request, 'chat/index.html', {'messages': chatMessages})
 
 def login_view(request):
+    redirect = request.GET.get('next')
     if request.method == 'POST':
         user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
 
         if user:
             login(request, user)
-            return HttpResponseRedirect('/chat/')
+            return HttpResponseRedirect(request.POST.get('redirect'))
         else:
-            return render(request, 'auth/login.html', {'wrongPassword': True})    
-    return render(request, 'auth/login.html')
+            return render(request, 'auth/login.html', {'wrongPassword': True, 'redirect': redirect})    
+    return render(request, 'auth/login.html', {'redirect': redirect})
