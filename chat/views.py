814bdcf50061aa67d4a45f03b495d4275d400aea
Junus Ergin <junus91@googlemail.com>
2021-12-13 12:12:23 +0100

    added login view

--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,7 @@
+from django.http.response import HttpResponseRedirect
 from django.shortcuts import render
 from .models import Chat, Message
+from django.contrib.auth import authenticate, login
 
 def index(request):
     if request.method == 'POST':
@@ -8,3 +10,14 @@ def index(request):
         Message.objects.create(text=request.POST['textmessage'], chat=myChat, author=request.user, receiver=request.user)
     chatMessages = Message.objects.filter(chat__id=1)
     return render(request, 'chat/index.html', {'messages': chatMessages})
+
+def login_view(request):
+    if request.method == 'POST':
+        user = authenticate(username=request.POST.get('username'), password=request.POST.get('password'))
+
+        if user:
+            login(request, user)
+            return HttpResponseRedirect('/chat/')
+        else:
+            return render(request, 'auth/login.html', {'wrongPassword': True})    
+    return render(request, 'auth/login.html')
--- django_chat_app/urls.py
+++ django_chat_app/urls.py
@@ -16,9 +16,10 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
-from chat.views import index
+from chat.views import index, login_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('chat/', index),
+    path('login/', login_view),
 ]
